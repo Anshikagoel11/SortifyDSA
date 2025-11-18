d987312368601fef6e443f6f3e82d2595c5bb22b
AnshikaGoel <11402anshikagoyal@gmail.com>
2025-11-18 21:38:55 +0530

    fix chatbot server issues

--- src/Backend/model.js
+++ src/Backend/model.js
@@ -51,7 +51,8 @@ app.post('/gemini/askDoubt', async (req, res) => {
     //config gemini
    
     const model = genAI.getGenerativeModel({
-  model: 'gemini-1.5-flash-001', 
+  model: 'gemini-1.5-flash-latest',
+
 });
 
 
